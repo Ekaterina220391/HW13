1c108c4787c397752f3ccdb202f92b6f3bd559c5
origin <dubnitskaya.katya@mail.ru>
2025-12-02 18:56:46 +0400

    New

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,67 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static class Book {
+        String title;
+        String author;
+        int date;
 
+        public Book(String title, String author, int date) {
+            this.title = title;
+            this.author = author;
+            this.date = date;
+        }
 
+        public String getTitle() {
+            return this.title;
+        }
+
+        public String getAuthor() {
+            return this.author;
+        }
+
+        public int getDate() {
+            return this.date;
+        }
+
+        public void setDate(int date) {
+            this.date = date;
+        }
     }
+
+    public static class Author {
+        String name;
+        String surname;
+
+        public Author(String name, String surname) {
+            this.name = name;
+            this.surname = surname;
+        }
+    }
+
+    public static void main(String[] args) {
+        Author author = new Author("Joan", "Rouling");
+        Author author1 = new Author("Lev", "Tolstoy");
+        System.out.println("Имя  " + author.name);
+        System.out.println("Фамилия " + author.surname);
+        System.out.println("Имя  " + author1.name);
+        System.out.println("Фамилия " + author1.surname);
+        Book book = new Book("Harry Potter", "Joan Rouling", 1997);
+        System.out.println("Название книги = " + book.getTitle());
+        System.out.println("Автор = " + book.getAuthor());
+        System.out.println("Дата написания произведения =" + book.getDate());
+        book.setDate(1999);
+        System.out.println("Измененная дата = " + book.getDate());
+        Book book1 = new Book("War and Peace", "Lev Tolstoy", 1873);
+        System.out.println("Название книги = " + book1.getTitle());
+        System.out.println("Автор = " + book1.getAuthor());
+        System.out.println("Дата написания произведения =" + book1.getDate());
+        book1.setDate(1875);
+        System.out.println("Измененная дата = " + book1.getDate());
+    }
+
+
 }
+
+
